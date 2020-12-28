994adcf212a7a1c589362461f498ae33af0581e3
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-12-27 20:34:41 -0700

    feat: more utils

--- utils/utils.js
+++ utils/utils.js
@@ -15,7 +15,7 @@ export let getPlanetRank = (planet) => {
 
 export let getSilverNeeded = (planet) => {
   if (!planet) return 0;
-  const totalLevel = planet.upgradeState.reduce((a, b) => a + b);
+  let totalLevel = planet.upgradeState.reduce((a, b) => a + b);
   return Math.floor((totalLevel + 1) * 0.2 * planet.silverCap);
 };
 
@@ -23,3 +23,49 @@ export let getSilver = (planet) => {
   if (!planet) return 0;
   return Math.floor(planet.silver);
 };
+
+export let BiomeNames = [
+  'Unknown',
+  'Ocean',
+  'Forest',
+  'Grassland',
+  'Tundra',
+  'Swamp',
+  'Desert',
+  'Ice',
+  'Wasteland',
+  'Lava',
+];
+
+export let emptyAddress = "0x0000000000000000000000000000000000000000";
+
+export let isUnowned = (planet) => planet.owner === emptyAddress;
+
+export let isMine = (planet) => planet.owner === df.account;
+
+export let unlockTimestamp = (planet) => {
+  return (planet.artifactLockedTimestamp + (12 * 60 * 60)) * 1000;
+}
+
+export let unlockTime = (planet) => {
+  return (new Date(unlockTimestamp(planet))).toLocaleString();
+}
+
+export let canWithdraw = (planet) => {
+  if (planet && planet.artifactLockedTimestamp) {
+    return Date.now() > unlockTimestamp(planet)
+  } else {
+    return false;
+  }
+}
+
+export let energy = (planet) => {
+  return Math.floor(planet.energy / planet.energyCap * 100);
+}
+
+export let canHaveArtifact = (planet) => {
+  return df.isPlanetMineable(planet) && !planet.hasTriedFindingArtifact
+}
+
+export let canFindArtifact = (planet) => energy(planet) >= 95;
+export let hasArtifact = (planet) => planet.heldArtifactId != null;
