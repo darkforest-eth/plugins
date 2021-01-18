7ea7134102ca9c2d8ec36a8b7e235fb68581c666
Philipp Wambach <philippwambach@googlemail.com>
2021-01-18 18:07:38 +0100

    Update readme and add readme text to plugin file

--- content/casual/hide-small-planets/plugin.js
+++ content/casual/hide-small-planets/plugin.js
@@ -1,3 +1,9 @@
+// # Hide small planets
+// This plugin overwrites the zoom based detail level of the map so that smaller planets can be hidden even on higher zoom levels.
+// Planets with an owner are preferred. Everything goes back to normal when the plugin window is closed.
+// Please note that small planets always flash for a second while zooming - didn't find a workaround for it yet.
+// Hiding planets can also improve rendering performance especially with slower GPUs by reducing the number of draw calls.
+
 class Plugin {
   constructor() {
     this.level = Math.max(0, ui.getDetailLevel() - 1);
