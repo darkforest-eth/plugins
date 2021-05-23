6bb6e27c9a0eb753873eec790b3384ef5c87621c
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-05-23 10:16:44 -0700

    Apply suggestions from code review
    Co-authored-by: Jacob Rosenthal <jacobrosenthal@gmail.com>

--- content/productivity/remote-explore/plugin.js
+++ content/productivity/remote-explore/plugin.js
@@ -3,7 +3,7 @@
 // The remote explore plugin allows us to use headless explorers that we run on
 // servers and RaspberryPi devices.
 //
-// We use [darkforest-rs](https://github.com/projectsophon/darkforest-rs) as a
+// We use [mimc-fast](https://github.com/projectsophon/darkforest-rs/tree/main/mimc-fast) as a
 // webserver that exposes a `/mine` endpoint and connect to it from in-game with
 // this plugin.
 //
@@ -365,4 +365,3 @@ class RemoteExplorerPlugin {
 }
 
 export default RemoteExplorerPlugin;
-
