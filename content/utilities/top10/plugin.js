570fc0fd73d115a35088b629a9c6b24fc5bc7fd4
0x-bear <85244339+0x-bear@users.noreply.github.com>
2021-06-30 16:01:18 -0400

    declare variable newRow
    Co-authored-by: Jacob Rosenthal <jacobrosenthal@gmail.com>

--- content/utilities/top10/plugin.js
+++ content/utilities/top10/plugin.js
@@ -16,7 +16,7 @@ let players=df.getAllPlayers().sort((p1,p2)=>{if (p1.withdrawnSilver + p1.totalA
 .slice(0,10)
 .forEach(function(item,index,array){
   if (item.twitter == undefined) { name = item.address.substr(0,5); } else { name = item.twitter }
-  newRow = [name,Math.round(item.withdrawnSilver/1000),Math.round(item.totalArtifactPoints/1000),Math.round((item.withdrawnSilver+item.totalArtifactPoints)/1000)]
+  let newRow = [name,Math.round(item.withdrawnSilver/1000),Math.round(item.totalArtifactPoints/1000),Math.round((item.withdrawnSilver+item.totalArtifactPoints)/1000)]
   table.push(newRow);
 });
 var text = "<style>tr td { border-bottom: 1px solid black; }</style><table cellpadding=3><tr><th></th><th colspan=3 align=center><b>score</th></tr><tr><td><b>name</td><td><b>silver</td><td><b>artifact</td><td><b>total</td></tr><tr><td>"+table.map(function(d){
