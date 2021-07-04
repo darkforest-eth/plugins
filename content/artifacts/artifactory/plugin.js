505c2df80948ce5dec31a26d93a3d52b3a728d28
ericet <chengchen369@yahoo.com>
2021-07-04 19:40:11 -0400

    Filtering out the planets which have no actual artifacts to prospect

--- content/artifacts/artifactory/plugin.js
+++ content/artifacts/artifactory/plugin.js
@@ -245,7 +245,7 @@ function Unfound({ selected }) {
   let [lastLocationId, setLastLocationId] = useState(null);
 
   let planets = myPlanetsWithFindable()
-    .filter(planet => !planet.hasTriedFindingArtifact);
+    .filter(planet => !planet.hasTriedFindingArtifact && !planet.unconfirmedProspectPlanet && planet.prospectedBlockNumber === undefined ) ;
 
   let planetsChildren = planets.map(planet => {
     let planetEntry = {
