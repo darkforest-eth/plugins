f3feaed051de0291f33b2da049118035b1f8a06b
zhaochunyou <snowtigersoft@126.com>
2022-02-24 11:23:16 +0800

    Improve voyage plugin to support energy estimate

--- content/utilities/voyage-time/plugin.js
+++ content/utilities/voyage-time/plugin.js
@@ -5,18 +5,23 @@
 let oneMinute = 60;
 let oneHour = 60 * oneMinute;
 
+
 class VoyageTime {
   constructor() {
+    this.energyPercent = 55;
     this.estimatedTime = document.createElement('div');
     this.estimatedTime.innerText = '?????';
     this.estimatedTime.style.textAlign = 'center';
     this.estimatedTimeSeconds = document.createElement('div');
     this.estimatedTimeSeconds.innerText = '';
     this.estimatedTimeSeconds.style.textAlign = 'center';
+    this.estimatedEnergy = document.createElement('div');
+    this.estimatedEnergy.innerText = '?????';
+    this.estimatedEnergy.style.textAlign = 'center';
   }
 
   calcVoyageTime = () => {
-    let fromPlanet = ui.getMouseDownPlanet();
+    let fromPlanet = ui.getSelectedPlanet();
     if (fromPlanet) {
       let toPlanet = ui.getHoveringOverPlanet();
 
@@ -36,19 +41,40 @@ class VoyageTime {
           this.estimatedTime.innerText = `${seconds} secs`;
         }
         this.estimatedTimeSeconds.innerText = `${time} secs`;
+        // Energy
+        let sendEnergy = (fromPlanet.energyCap * this.energyPercent / 100);
+        let arriveEnergy = df.getEnergyArrivingForMove(fromPlanet.locationId, toPlanet.locationId, undefined, sendEnergy);
+        if (toPlanet.owner !== df.getAccount()) {
+          arriveEnergy = arriveEnergy * 100 / toPlanet.defense
+        }
+        sendEnergy = Math.ceil(sendEnergy);
+        arriveEnergy = Math.ceil(arriveEnergy);
+        if (sendEnergy > 1000000) {
+          sendEnergy = (sendEnergy / 1000000).toFixed(1) + 'M';
+        } else if (sendEnergy > 1000) {
+          sendEnergy = (sendEnergy / 1000).toFixed(1) + 'K';
+        }
+        if (arriveEnergy > 1000000) {
+          arriveEnergy = (arriveEnergy / 1000000).toFixed(1) + 'M';
+        } else if (arriveEnergy > 1000) {
+          arriveEnergy = (arriveEnergy / 1000).toFixed(1) + 'K';
+        }
+        this.estimatedEnergy.innerText = `Send ${sendEnergy} arrive ${arriveEnergy}`;
       } else {
         this.estimatedTime.innerText = '?????';
         this.estimatedTimeSeconds.innerText = ``;
+        this.estimatedEnergy.innerText = '?????';
       }
     } else {
       this.estimatedTime.innerText = '?????';
       this.estimatedTimeSeconds.innerText = ``;
+      this.estimatedEnergy.innerText = '?????';
     }
   }
 
   render(container) {
     container.parentElement.style.minHeight = 'unset';
-    container.style.width = '200px';
+    container.style.width = '250px';
     container.style.minHeight = 'unset';
     window.addEventListener('mousemove', this.calcVoyageTime);
 
@@ -59,6 +85,38 @@ class VoyageTime {
     container.appendChild(label);
     container.appendChild(this.estimatedTime);
     container.appendChild(this.estimatedTimeSeconds);
+
+    let stepperLabel = document.createElement('label');
+    stepperLabel.innerText = 'Estimated energy spend:';
+    stepperLabel.style.textAlign = 'center';
+    stepperLabel.style.display = 'block';
+
+    let stepper = document.createElement('input');
+    stepper.type = 'range';
+    stepper.min = '0';
+    stepper.max = '100';
+    stepper.step = '5';
+    stepper.value = `${this.energyPercent}`;
+    stepper.style.width = '80%';
+    stepper.style.height = '24px';
+
+    let percent = document.createElement('span');
+    percent.innerText = `${stepper.value}%`;
+    percent.style.float = 'right';
+
+    stepper.onchange = (evt) => {
+      percent.innerText = `${evt.target.value}%`;
+      try {
+        this.energyPercent = parseInt(evt.target.value, 10);
+      } catch (e) {
+        console.error('could not parse energy percent', e);
+      }
+    }
+
+    container.appendChild(stepperLabel);
+    container.appendChild(stepper);
+    container.appendChild(percent);
+    container.appendChild(this.estimatedEnergy);
   }
 
   destroy() {
